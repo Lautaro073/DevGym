35948acc001fda86df431712fdc572f22f5d28eb
Lautaro073 <lautarojimene02@gmail.com>
2023-05-06 03:01:26 -0300

    se agrego .zip del node_module para poder usar el npm start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import Nav from'./nav.jsx'
 import Footer from'./footer.jsx'
 import Header from'./header.jsx'
+import Principal from'./principal.jsx'
 function App() {
   return (
 <div className="container">
       
       <Header></Header>
       <Nav></Nav>
+      <Principal></Principal>
        <Footer></Footer>
       
     </div>
