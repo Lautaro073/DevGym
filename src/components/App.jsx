b4c40a1c5b147748d616bcb8892a427773a151a8
Lautaro073 <lautarojimene02@gmail.com>
2023-05-10 21:24:22 -0300

    realice el footerV1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,9 @@ function App() {
   return (
 <div className="">
       
-      <Header></Header>
-      <Nav></Nav>
-      <Principal></Principal>
+   <Header></Header>
+   <Nav></Nav>
+ <Principal></Principal>
        <Footer></Footer>
       
     </div>
