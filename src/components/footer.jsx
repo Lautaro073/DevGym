ad42c405b06f22ddbfb01f67215551bc9f82b5ed
Lautaro073 <lautarojimene02@gmail.com>
2023-05-27 22:03:34 -0300

    detallitos

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -9,27 +9,27 @@ const Footer = () => {
           <img src={logoFooter}></img>
         </figure>
         </div>
-        <div class="box">
+        <div className="box">
           <h2>CLASES</h2>
           <p>CrossFit</p>
           <p>Musculación</p>
           <p>Pilates</p>
         </div>
-        <div class="box">
+        <div className="box">
           <h2>HORARIOS</h2>
           <p>Lunes a Viernes: 7 a 23 hs</p>
           <p>Sábado: 9 a 18 hs</p>
          
         </div>
       </div>
-      <div class="grupo-2">
-        <div class="box">
+      <div className="grupo-2">
+        <div className="box">
           <h2>SIGUENOS</h2>
-          <div class="red-social">
-            <a href="" class="fa fa-facebook"></a>
-            <a href="" class="fa fa-instagram"></a>
-            <a href="" class="fa fa-twitter"></a>
-            <a href="" class="fa fa-linkedin"></a>
+          <div className="red-social">
+            <a href="" className="fa fa-facebook"></a>
+            <a href="" className="fa fa-instagram"></a>
+            <a href="" className="fa fa-twitter"></a>
+            <a href="" className="fa fa-linkedin"></a>
           </div>
         </div>
         <small>
